308205c528e760e2d453651de989c41d0503ce06
Furtif <msntchat@hotmail.fr>
2018-08-02 02:24:50 +0200

    test

--- DraconiusGoGUI/GoManager/Manager.Building.cs
+++ DraconiusGoGUI/GoManager/Manager.Building.cs
@@ -45,7 +45,7 @@ namespace DraconiusGoGUI.DracoManager
 
             try
             {
-                response = await _client.DracoClient.TryUseBuildingAsync(Building.id, Building.coords.latitude, Building.coords.longitude, Building.dungeonId);
+                response = _client.DracoClient.TryUseBuilding(Building.id, Building.coords.latitude, Building.coords.longitude, Building.dungeonId);
             }
             catch (Exception ex)
             {
