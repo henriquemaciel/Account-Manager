02d7b3184b894527bf9e9f6d18481ae662c73374
Furtif <msntchat@hotmail.fr>
2018-08-03 22:43:39 +0200

    update comment

--- DraconiusGoGUI/GoManager/Manager.Navigation.cs
+++ DraconiusGoGUI/GoManager/Manager.Navigation.cs
@@ -150,8 +150,8 @@ namespace DraconiusGoGUI.DracoManager
                 // In this point we are sure that we are mimic walking but dont know if enable humazation is true
                 var delay = CalculateDelay(UserSettings.DelayBetweenLocationUpdates, UserSettings.LocationupdateDelayRandom);
                 
-                // if enable humanization is true, then delay is 0 so we put a minimum delay of 10 seconds.
-                delay = (delay ==0)? 10000 + new Random().Next(0, 200): delay; 
+                // if enable humanization is false, then delay is 0 so we put a minimum delay of 10 seconds.
+                delay = (delay == 0) ? 10000 + new Random().Next(0, 200): delay; 
                 await Task.Delay(delay);
 
                 if (!result.Success)
